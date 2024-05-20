60619ccd62fd28e5a4943a4579d5991db4459b73
Totile <chfrodon@gmail.com>
2024-05-20 16:48:21 +0200

    2 - feat: polling gamepad status

--- showroom/src/App.vue
+++ showroom/src/App.vue
@@ -28,7 +28,6 @@ export default {
   },
   methods: {
     connectGamepad(event) {
-      console.log("from GlobalEvent")
       console.log(
         "Gamepad connected at index %d: %s. %d buttons, %d axes.",
         event.gamepad.index,
@@ -36,26 +35,49 @@ export default {
         event.gamepad.buttons.length,
         event.gamepad.axes.length,
       );
-      this.gamepad = event.gamepad
+      this.gamepad = navigator.getGamepads()[event.gamepad.index];
+      this.pollGamepad();
+      console.log("poll started")
+    },
+    disconnectGamepad(event) {
+      this.gamepad = null
+      clearInterval(this.pollGamepad)
+      console.log("Gamepad disconnected, poll ended")
     },
     showGamepadStatus() {
-      console.log("clicked on main button")
-      console.log(this.gamepad.id)
+      console.log(this.gamepad.buttons)
+      console.log(this.gamepad.buttons[2])
+      console.log(this.gamepad.buttons[0].pressed)
+    },
+    pollGamepad() {
+      this.pollGamepad = setInterval(() => {
+        this.gamepad = navigator.getGamepads()[0];
+        // console.log(this.gamepad)
+      }, 25)
+    },
+  },
+  computed: {
+    buttons() {
+      return this.gamepad.buttons
     }
+  },
+  beforeDestroy() {
+    clearInterval(this.pollGamepad)
   }
 }
 </script>
 
 
 <template class="container">
-  <GlobalEvents target="window" @gamepadconnected="connectGamepad" />
+  <GlobalEvents target="window" @gamepadconnected="connectGamepad" @gamepaddisconnected="disconnectGamepad" />
   <div class="row">
     <div class="col">
       <img alt="Vue logo" src="./assets/logo.svg" width="125" height="125" />
 
     </div>
     <div class="col">
-      <button class="btn btn-danger alert alert-danger" :disabled="!gamepad" @click="showGamepadStatus">
+      <button class="btn btn-danger alert alert-danger" :disabled="!gamepad || !gamepad.connected"
+        @click="showGamepadStatus">
         btn1</button>
     </div>
 
