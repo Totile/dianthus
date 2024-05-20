1dd25800e92e9f9d1638d55974cc84399df6ebb4
Totile <chfrodon@gmail.com>
2024-05-20 14:45:48 +0200

    #1 - refac: further refactor of button loop definition

--- controller/src/main.cpp
+++ controller/src/main.cpp
@@ -7,12 +7,6 @@ constexpr int lampCount = 6;
 constexpr int btnCount = 8;
 const int lampPin[lampCount] = {10, 14, A0, A2, 9, 4};
 const int btnPin[btnCount] = {16, 15, A1, A3, 8, 7, 6, 5};
-const int PIN_LED1 = 3;
-const int PIN_BTN1 = 15;
-const int PIN_LED2 = 4;
-const int PIN_BTN2 = 14;
-const int PIN_LED3 = 5;
-const int PIN_BTN3 = 16;
 
 // Joystick initialisation
 // TODO try to set it as a Gamepad, not a joystick
@@ -34,16 +28,7 @@ void buttonHandler(uint8_t btnID, uint8_t btnSTate) {
     }
 }
 
-static Button btn1(0, buttonHandler);
-static Button btn2(1, buttonHandler);
-static Button btn3(2, buttonHandler);
-static Button btn4(3, buttonHandler);
-static Button btn5(4, buttonHandler);
-static Button btn6(5, buttonHandler);
-static Button btn7(6, buttonHandler);
-static Button btn8(7, buttonHandler);
-static Button btnArr[btnCount] = {btn1, btn2, btn3, btn4,
-                                  btn5, btn6, btn7, btn8};
+Button btnArr[btnCount] = {};
 
 void setup() {
     for (int i = 0; i < btnCount; i++) {
@@ -51,6 +36,10 @@ void setup() {
     }
     for (int i = 0; i < lampCount; i++) {
         pinMode(lampPin[i], OUTPUT);
+    }
+    for (int i = 0; i < btnCount; i++) {
+        const Button tempBtn(i, buttonHandler);
+        btnArr[i] = tempBtn;
     }
 
     Serial.begin(9600);
