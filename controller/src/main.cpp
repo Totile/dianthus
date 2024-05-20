9d4cd3a88d660ac1c33c2f8589bf3e841c3d3756
Totile <chfrodon@gmail.com>
2024-05-20 11:00:05 +0200

    dev - refac: pin and lamp mode in for loop

--- controller/src/main.cpp
+++ controller/src/main.cpp
@@ -24,13 +24,13 @@ void buttonHandler(uint8_t btnID, uint8_t btnSTate) {
         Serial.print("Button ");
         Serial.print(btnID);
         Serial.println(" pressed");
-        digitalWrite(PIN_LED1, 1);
+        digitalWrite(lampPin[0], 1);
     } else {
         Joystick.releaseButton(btnID);
         Serial.print("Button ");
         Serial.print(btnID);
         Serial.println(" released");
-        digitalWrite(PIN_LED1, 0);
+        digitalWrite(lampPin[0], 0);
     }
 }
 
@@ -46,32 +46,24 @@ static Button btnArr[btnCount] = {btn1, btn2, btn3, btn4,
                                   btn5, btn6, btn7, btn8};
 
 void setup() {
-    pinMode(btnPin[0], INPUT_PULLUP);
-    pinMode(btnPin[1], INPUT_PULLUP);
-    pinMode(btnPin[2], INPUT_PULLUP);
-    pinMode(btnPin[3], INPUT_PULLUP);
-    pinMode(btnPin[4], INPUT_PULLUP);
-    pinMode(btnPin[5], INPUT_PULLUP);
-    pinMode(btnPin[6], INPUT_PULLUP);
-    pinMode(btnPin[7], INPUT_PULLUP);
-    pinMode(PIN_LED1, OUTPUT);
+    for (int i = 0; i < btnCount; i++) {
+        pinMode(btnPin[i], INPUT_PULLUP);
+    }
+    for (int i = 0; i < lampCount; i++) {
+        pinMode(lampPin[i], OUTPUT);
+    }
 
     Serial.begin(9600);
     Joystick.begin();
 }
 
 static void pollButtons() {
-    btn1.update(digitalRead(btnPin[0]));
-    btn2.update(digitalRead(btnPin[1]));
-    btn3.update(digitalRead(btnPin[2]));
-    btn4.update(digitalRead(btnPin[3]));
-    btn5.update(digitalRead(btnPin[4]));
-    btn6.update(digitalRead(btnPin[5]));
-    btn7.update(digitalRead(btnPin[6]));
-    btn8.update(digitalRead(btnPin[7]));
+    for (int i = 0; i < btnCount; i++) {
+        btnArr[i].update(digitalRead(btnPin[i]));
+    }
 }
 
 void loop() {
     pollButtons();
-    delay(25);
+    delay(1);
 }
